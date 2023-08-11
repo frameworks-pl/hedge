d5fdf838bb1fb9938f26f9bc7d887e706b5f1809
Janusz Grabis <janusz@frameworks.pl>
2023-08-11 06:34:49 +0200

    Ignore colorama if not present (true)

--- src/lib/log.py
+++ src/lib/log.py
@@ -1,9 +1,16 @@
-from colorama import init, Fore, Back, Style
+try:
+    from colorama import init, Fore, Back, Style
+except ImportError:
+    pass
+import logging
 
 class Log:    
 
     def __init__(self, autoFlush = True):
-        init(autoreset=True)
+        try:
+            init(autoreset=True)
+        except:
+            logging.warning("Colorama module not found. Colored output will not be available.")
         self.autoFlush = autoFlush
 
     def addPending(self, entry):
@@ -22,6 +29,10 @@ class Log:
     def flush(self):
         output = "{entry} {result}".format(entry=self.entry,result=self.result)        
         c = Fore.GREEN if self.result == "OK" else Fore.RED
+        try:
+            c = Fore.GREEN if self.result == "OK" else Fore.RED
+        except:
+            pass
         print(self.entry + " " + c + self.result)
         self.entry = None
         return output
