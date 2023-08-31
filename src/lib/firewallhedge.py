efa034806949dc3b31a6056b8a923e4cc4c9e9d0
Janusz Grabis <janusz@frameworks.pl>
2023-08-31 10:36:37 +0200

    Adding rejection rule works

--- src/lib/firewallhedge.py
+++ src/lib/firewallhedge.py
@@ -3,5 +3,43 @@ import shutil
 import os
 from basehedge import BaseHedge
 
-class FirewallHedge(BaseHedge):
-    pass
+class FirewallHedge(object):
+    
+    def addInputRejectionRule(self, ip, port = None, protocol = "all"):
+        """
+            Adds a rule to reject all incoming traffic from specified IP address
+            
+            Args:
+                ip (str): IP address
+                port (int): Port number
+            Returns:
+                void
+        """
+        self._addRule('INPUT', 'REJECT', ip, port, protocol)
+
+    def _addRule(self, chain, action, ip, port, protocol):
+        """
+            Adds a rule to specified chain
+            
+            Args:
+                chain (str): Chain name
+                ip (str): IP address
+                port (int): Port number
+                protocol (str): Protocol name
+            Returns:
+                Boolean: True if rule was added, False otherwise
+        """
+
+        cmd = "iptables -A {chain}".format(chain=chain)
+        if (protocol != None):
+            cmd += " -p {protocol}".format(protocol=protocol)
+
+        cmd += " -s {ip}".format(ip=ip)
+
+        if (port != None):
+            cmd += " --dport {port}".format(port=str(port))
+
+        cmd += " -j {action}".format(action=action)
+        print(cmd)
+        return os.system(cmd) == 0
+      
--- test/test_firewallhedge.py
+++ test/test_firewallhedge.py
@@ -5,11 +5,24 @@ sys.path.insert(0, srcFolder)
 libFolder = os.path.realpath(os.getcwd() + '/../src/lib')
 sys.path.insert(0, libFolder)
 from test_base import TestBase
-from agent import Agent
+from firewallhedge import FirewallHedge
 
 class TestFirewallHedge(TestBase):
     
     def testSimpleRejectRule(self):
-        firewall = FireWallHedge(self)
-        firewall.addRejectRule('10.0.0.10/24')
 
+        # If the rule IS present remove it before the test
+        if os.system('iptables --check INPUT -p all -s 10.0.0.10/32 -j REJECT >> /dev/null') != 0:
+            os.system('iptables -D INPUT -p all -s 10.0.0.10/32 -j REJECT >> /dev/null')
+            
+        # Rule is NOT present
+        assert(os.system('iptables --check INPUT -p all -s 10.0.0.10/32 -j REJECT >> /dev/null') != 0)
+
+        firewall = FirewallHedge()
+        firewall.addInputRejectionRule('10.0.0.10/32')
+
+        assert(os.system('iptables --check INPUT -p all -s 10.0.0.10/32 -j REJECT >> /dev/null') == 0)
+
+
+if __name__ == '__main__':
+    unittest.main()  
